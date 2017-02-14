973f4447fdc452b9bfc87df72c4d12d79539249f
Shi Jijun <5080053@qq.com>
2017-02-14 15:17:37 +0800

    test

---
+++ src/basic/test/JavaBasicTest.java
@@ -0,0 +1,15 @@
+package basic.test;
+
+/*Java中各个进制的表示. 
+ * 
+ * */
+public class JavaBasicTest {
+	public static void main(String[] args) {
+		int a = 100; //十进制
+		System.out.println(a);
+		a = 0144; //八进制
+		System.out.println(a);
+		a = 0xFF; //十六八进制
+		System.out.println(a);
+	}
+}
