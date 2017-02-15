48f0148d85a5e0946b06291f85f60200021e3ff2
Shi Jijun <5080053@qq.com>
2017-02-15 14:07:40 +0800

    test

--- src/basic/test/JavaBasicTest.java
+++ src/basic/test/JavaBasicTest.java
@@ -1,5 +1,7 @@
 package basic.test;
 
+import java.nio.ByteBuffer;
+
 /*Java中各个进制的表示. 
  * 
  * */
@@ -11,5 +13,21 @@ public class JavaBasicTest {
 		System.out.println(a);
 		a = 0xFF; //十六八进制
 		System.out.println(a);
+		
+		int b = 255; 
+		
+		//Integer.toHexString
+		System.out.println(Integer.toHexString(b));
+		
+		//直接打印每个byte，会导致打印成int
+		byte[] bytes = ByteBuffer.allocate(4).putInt(b).array();
+		for (byte c : bytes) {
+			System.out.println( c);
+		}
+		
+		//每个byte打印成十六进制格式
+		for (byte c : bytes) {
+		   System.out.format("0x %x ", c);
+		}
 	}
 }
